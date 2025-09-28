de3e0c3bf494edb6669007f218f65d8eaf18df33
vishwachary <vishwachary.varanasi@example.com>
2025-09-28 15:06:29 +0530

    initla files

---
+++ src/test/java/org/example/mybookstore/MyBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mybookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
